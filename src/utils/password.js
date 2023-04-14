c99b113069fd57b653b4414f37234bd2ad11ad76
Pedro Viana <pedroviana.developer13@gmail.com>
2023-04-14 17:18:07 -0300

    fix[passValidator]: add o status 400 em uma const

--- src/utils/password.js
+++ src/utils/password.js
@@ -1,14 +1,16 @@
+const BAD_REQUEST_STATUS = 400;
+
 function passValidator(req, res, next) {
     const { password } = req.body;
 
     if (!password) {
-        return res.status(400).json({
+        return res.status(BAD_REQUEST_STATUS).json({
             message: 'O campo "password" é obrigatório',
         });
     }
 
     if (password.length < 6) {
-        return res.status(400).json({
+        return res.status(BAD_REQUEST_STATUS).json({
             message: 'O "password" deve ter pelo menos 6 caracteres',
         });
     }
