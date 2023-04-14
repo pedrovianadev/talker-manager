7feb0b303c956fab4ad4fd70eef1699e77a4ae4d
Pedro Viana <pedroviana.developer13@gmail.com>
2023-04-14 15:18:27 -0300

    feat[requisito2]: Criado o endpoint GET /talker:id

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
+const NOT_FOUND_STATUS = 404;
 const PORT = process.env.PORT || '3001';
 const talkers = path.resolve(__dirname, './talker.json');
 
@@ -22,4 +23,17 @@ app.get('/talker', async (req, res) => {
   const talker = JSON.parse(await fs.readFile(talkers, 'utf-8'));
 
   return res.status(HTTP_OK_STATUS).json(talker);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const talker = JSON.parse(await fs.readFile(talkers, 'utf-8'));
+
+  const findId = talker.find(({ id }) => id === Number(req.params.id));
+
+  if (!findId) {
+    return res.status(NOT_FOUND_STATUS).send({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  }
+  return res.status(HTTP_OK_STATUS).json(findId);
 });
