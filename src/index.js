9b611906ae003bdcaa24b62afe1e858d497a008c
Pedro Viana <pedroviana.developer13@gmail.com>
2023-04-14 14:49:16 -0300

    feat[requisito1]: Criado o endpoint GET /talker

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
-
-// apenas para o commit inicial
+const talkers = path.resolve(__dirname, './talker.json');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -16,3 +17,9 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+app.get('/talker', async (req, res) => {
+  const talker = JSON.parse(await fs.readFile(talkers, 'utf-8'));
+
+  return res.status(HTTP_OK_STATUS).json(talker);
+})
