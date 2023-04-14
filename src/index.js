e4a3e4c9990713113198e09490e65f004cc06eb7
Pedro Viana <pedroviana.developer13@gmail.com>
2023-04-14 14:33:33 -0300

    iniciando o projeto

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
+// apenas para o commit inicial
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
